ea00c7898ba26640304bb4e283a502509d88c86f
Christian Vaikona <christianvaikona@gmail.com>
2026-01-09 19:01:39 -0800

    Learning nba_api
    Still going through and trying to understand the nba_api and trying to understand what I need for this project.

--- data_ingest.py
+++ data_ingest.py
@@ -1,5 +1,16 @@
-import pandas as pd
-from sqlalchemy import create_engine
-from nba_api.stats.endpoints import leagueleaders
+from nba_api.stats.static import players
+player_dict = players.get_players()
+from nba_api.stats.static import teams
+teams = teams.get_teams()
+from nba_api.stats.static import teams
+teams = teams.get_teams()
+from nba_api.stats.endpoints import shotchartdetail, leaguedashplayershotlocations
 
-print("hello world")
+
+
+shot_detail = shotchartdetail.ShotChartDetail(player_id=0, 
+              team_id=0, context_measure_simple = 'FGA',     
+              season_type_all_star='Playoffs')
+shot_df = shot_detail.get_data_frames()[0]
+
+print(shot_df.head())
